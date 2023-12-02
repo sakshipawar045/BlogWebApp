c1c2939ea995fa6552a242c792c2a8783acd8ee1
sakshi pawar <pawar272003@gmail.com>
2023-12-02 22:29:02 +0530

    Your descriptive commit message here

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "12345", 
+    resave: false,
+    saveUninitialized: false,
+    
+  })
+);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -26,6 +36,8 @@ const blogSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
+  email : String,
+  password : String
 
 });
 
@@ -33,7 +45,7 @@ const userSchema = new mongoose.Schema({
 // const userSchema = ...
 
 const Blog = new mongoose.model("Blog", blogSchema);
-
+const User = new mongoose.model("user", userSchema);
 // Hint: Create User Model here
 // const User = ...
 
@@ -42,6 +54,18 @@ app.get('/', (req, res) => {
   Blog.find({})
     .then((posts) => {
       res.render("index", { blogPosts: posts })
+    })
+    .catch((err) => {
+      console.log("Error getting data", err);
+      res.redirect("/");
+    });
+  // res.redirect("login");
+});
+
+app.get("/index", (req, res) => {
+  Blog.find({})
+    .then((posts) => {
+      res.render("index", { blogPosts: posts });
     })
     .catch((err) => {
       console.log("Error getting data", err);
@@ -70,7 +94,7 @@ app.post('/compose', (req, res) => {
       console.log("New Blog Posted");
     })
     .catch((err) => {
-      console.log("Error posting New Blog");
+      console.log("Error posting New Blog" , err);
     });
 
   res.redirect('/');
@@ -86,10 +110,16 @@ app.get('/post/:id', (req, res) => {
 
   Blog.findOne({ _id: reqID })
     .then((post) => {
-       // Guess it    
+      const showBlog = {
+        imageURL : post.imageURL,
+        title : post.title,
+        description : post.description
+      }
+
+      res.render("post", showBlog );
     })
     .catch((err) => {
-      console.log("Post Not Found");
+      console.log("Post Not Found ", err);
       res.redirect('/');
     });
 })
@@ -101,62 +131,95 @@ app.get('/post/delete/:id', (req, res) => {
 
   // TODO: Task 2 to implement the delete operation with mongodb
 
-  // const idToDelete = req.params.id;
+  const idToDelete = req.params.id;
 
   // Your Logic Here...
-  // Search mongoDB method to find and delete document using ID
-})
+  // Search mongoDB method to find and delete document using ID/
+  
+  Blog.deleteOne({_id : idToDelete})
+  .then((isdelete)=>{
+    if(isdelete.deletedCount == 1){
+      console.log("Post delted successfully ");
+    }
+    else{
+      console.log("Post not found ");
+    }
+    res.redirect("/");
+  })
+  .catch((err)=>{
+    console.log("Error while deleting post !!!", err );
+    res.redirect("/");
+  });
+});
 
 
 // TODO: Task 3 to implement login and sign up.
 
 // signup routes
 app.get("/signup", (req, res) => {
-  // render signup page
+  res.render("signup");
 });
 
 app.post("/signup", (req, res) => {
   // Hint:
-  // const newUser = new User({
-  //   email: type here something...,
-  //   password: type here something...,
-  // });
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
 
-  // newUser.save()
-  //   .then(() => {
-  //     console.log("New User Created");
-  //   })
-  //   .catch((err) => {
-  //     console.log("Error Creating New User");
-  //   });
+  newUser.save()
+    .then(() => {
+      console.log("New User Created");
+      
+    })
+    .catch((err) => {
+      console.log("Error Creating New User", err);
+    });
 
 
   // redirect to login page
+  res.redirect("/login")
 });
 
 // login routes
 app.get("/login", (req, res) => {
   // Hint
   // render login page
+  res.render("login");
 });
 
 app.post("/login", (req, res) => {
   // Hint
-  // const reqEmail = something...
-  // const reqPassword = something...
+  const reqEmail = req.body.email;
+  const reqPassword = req.body.password;
   //
-  // User.findOne({ email: reqEmail })
-  //   .then((user) => {
-  //     if (user.password == reqPassword) {
-  //       // Guess it!
-  //     } else {
-  //       // Guess it!
-  //     }
-  //   })
-  //   .catch((err) => {
-  //       console.log("User Not Found");
-  //       redirect to login page
-  //   });
+  User.findOne({ email: reqEmail })
+    .then((user) => {
+      if (user.password == reqPassword) {
+        req.session.isLoggedIn = true;
+        res.redirect("/index")
+        console.log("Login successful...");
+      } else {
+        res.redirect("/login");
+        console.log("Password does not match...")
+        
+      }
+    })
+    .catch((err) => {
+        console.log("User Not Found",err);
+        res.redirect("/login");
+        
+    });
+});
+
+app.get("/logout", (req, res) => {
+  // Clear the session and redirect to the login page
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("Error logging out:", err);
+    }
+    res.redirect("/login");
+  });
 });
 
 
